6b9c9363263a51e151f14593b4e1a3d2ab2a88e9
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-04-11 14:51:17 +0100

    After enums

---
+++ EnumExample/src/main/java/com/sparta/enums/EnumDriver.java
@@ -0,0 +1,31 @@
+package com.sparta.enums;
+
+import java.util.Scanner;
+
+public class EnumDriver {
+    public enum Day {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+//    public static final int MONDAY = 0;
+//    public static final int TUESDAY = 1;
+//    public static final int WEDNESDAY = 2;
+//    public static final int THURSDAY = 3;
+//    public static final int FRIDAY = 4;
+//    public static final int SATURDAY = 5;
+//    public static final int SUNDAY = 6;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // we should add some exception handling here
+        Day dayOfWeek = Day.valueOf(scan.next().toUpperCase());
+        if(dayOfWeek.equals(Day.MONDAY))
+            System.out.println("It's Monday :-(");
+        switch (dayOfWeek) {
+            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+                    -> System.out.println("It's a week day");
+            case SATURDAY, SUNDAY
+                    -> System.out.println("It's the weekend!");
+            default
+                    -> System.out.println("Invalid day");
+        }
+    }
+}
--- StringExample/src/main/java/com/sparta/strings/StringExample.java
+++ StringExample/src/main/java/com/sparta/strings/StringExample.java
@@ -2,6 +2,43 @@ package com.sparta.strings;
 
 public class StringExample {
     public static void main(String[] args) {
+        String andy = "Andy Marincas";
+        String alex = "Alex Costi";
+        System.out.println(alex.compareTo(alex));
+        System.out.println("SE1 0AA"
+                .matches("^[a-zA-Z]{1,2}[0-9][0-9A-Za-z]{0,1} {0,1}[0-9][A-Za-z]{2}$"));
+        String listOfNames = "Idriss/Reman/Anthony";
+        String[] nameArray = listOfNames.split("/");
+        for (String name: nameArray)
+            System.out.println(name);
+        System.out.println(listOfNames.substring(7, 12));
+    }
+
+    private static StringBuilder michel() {
+        String firstName = "Michel";
+        String lastName = "Jean";
+        // + can be used for concatenation
+        // unnecessary objects are created
+        System.out.println(firstName + " " + lastName);
+        System.out.println(firstName.concat(" ").concat(lastName));
+        firstName = firstName + "2";
+        // how not to do it
+        String result = "";
+        for(int i = 0; i < 1000; i++)
+            result = result + firstName + " " + lastName + " ";
+        System.out.println(result);
+        // better solution
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < 1000; i++){
+            builder.append(firstName);
+            builder.append(" ");
+            builder.append(lastName);
+            builder.append(" ");
+        }
+        return builder;
+    }
+
+    private static void usama() {
         // this String is created in the pool
         String usama = "Usama Navid"; // init using String literal
         // explicitly use the String constructor, stored in the heap
