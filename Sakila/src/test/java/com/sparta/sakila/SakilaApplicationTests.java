df245afbd42339b27971b0cf2d2d8e3abb115f26
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-05-11 16:26:57 +0100

    Update SakilaApplicationTests.java

--- Sakila/src/test/java/com/sparta/sakila/SakilaApplicationTests.java
+++ Sakila/src/test/java/com/sparta/sakila/SakilaApplicationTests.java
@@ -1,13 +1,11 @@
 package com.sparta.sakila;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SakilaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
