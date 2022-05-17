e0f15eef016c53230d46819117bd42c5da05bb9f
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-05-17 10:30:40 +0100

    Done edit kinda

--- SakilaWeb/src/main/java/com/sparta/sakilaweb/controller/SakilaController.java
+++ SakilaWeb/src/main/java/com/sparta/sakilaweb/controller/SakilaController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class SakilaController {
@@ -24,5 +28,31 @@ public class SakilaController {
         System.out.println(filmObj);
         model.addAttribute("filmAttr", filmObj); // store the film in the model
         return "filmView";
+    }
+    @GetMapping("film")
+    public String getAllFilms(Model model){
+        List<Film> result = filmRepo.findAll();
+        model.addAttribute("allFilmsList", result);
+        return "allFilms";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteFilm(@PathVariable int id, Model model){
+        filmRepo.deleteById(id);
+        model.addAttribute("id_deleted", id);
+        return "filmDeleted";
+    }
+    @GetMapping("/edit/{id}")
+    public String editFilm(@PathVariable int id, Model model){
+        Film thisFilm = filmRepo.getById(id);
+        model.addAttribute("filmToEdit", thisFilm);
+        return "editFilmForm";
+    }
+    @PostMapping("/updateFilm")
+    public String updateFilm(@ModelAttribute("filmToEdit") Film theFilm){
+        Film oldState = filmRepo.getById(theFilm.getId());
+        // replace the values in the old object with the new values from theFilm
+        // oldState.setTitle(theFilm.getTitle()); etc...
+        filmRepo.save(oldState);
+        return "editSuccess";
     }
 }
